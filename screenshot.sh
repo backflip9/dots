1fe75ba48aaeaa8cbf03565cae8173de3d88a977
Liam Schroth <schrothl@sonoma.edu>
2018-12-24 02:52:55 -0800

    delete copies

--- screenshot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-filename="/home/liam/Pictures/`date`.png"
-scrot "$filename"
-xclip -selection clipboard -t image/png -i "$filename"
-if [[ -f $filename ]]
-then
-  notify-send "fullscreen saved to $filename"
-fi
--- screenshot2-window.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-xfce4-screenshooter -w --clipboard
-filename="/home/liam/Pictures/`date`.png"
-xclip -selection clipboard -t image/png -o > "$filename"
-if [[ -f $filename ]]
-then
-  notify-send "selected window saved to $filename"
-fi
